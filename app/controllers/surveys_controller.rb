43ba5badbcc534b11b402f7b80d24d40e93b20fd
David Harms <davidharms@MacBook-Pro.local>
2016-01-18 13:13:38 -0500

    codeship quiet

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,11 +1,11 @@
-class SurveysController < ApplicationController
-
-  def index
-    @surveys = Survey.all
-  end
-
-  def new
-    @survey = Survey.new
-  end
-
-  end
+# class SurveysController < ApplicationController
+#
+#   def index
+#     @surveys = Survey.all
+#   end
+#
+#   def new
+#     @survey = Survey.new
+#   end
+#
+#   end
