57ae7efb123a3d286868f1ef9aea32f31ec669e4
David Harms <davidharms@Davids-MacBook-Pro-2.local>
2016-08-16 15:43:04 -0400

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+if Rails.env.production?
+admin = Admin.find_or_create_by!([{email: "yo@yo.com", id:10000000}])
+survey = Survey.find_or_create_by!([{ title: 'Example Survey', group: 'Example Group', publish: true, admin_id: admin.first.id }])
+questions = [Question.find_or_create_by!([{ body: "How much do you like this question on a scale of one to ten?", rating: true, survey_id: survey.first.id }]),
+ Question.find_or_create_by!([{ body: "Explain how you feel about this question?", text: true, survey_id: survey.first.id  }]),
+ Question.find_or_create_by!([{ body: "Explain and mark how much do you like this question on a scale of one to ten.", text: true, rating: true, survey_id: survey.first.id  }])]
+end
